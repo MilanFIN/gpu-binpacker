2772198f9e468419ebff23b158978214b201b25e
milanfin <milan.makipaa@gmail.com>
2025-11-28 22:00:14 +0200

    update tests

---
+++ src/test/java/com/binpacker/lib/solver/MOABTest.java
@@ -0,0 +1,33 @@
+package com.binpacker.lib.solver;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.binpacker.lib.common.Box;
+import com.binpacker.lib.common.Point3f;
+
+class MOABTest {
+
+	@Test
+	void testSolve() {
+		MOAB solver = new MOAB();
+		List<Box> boxes = new ArrayList<>();
+		boxes.add(new Box(1, new Point3f(0, 0, 0), new Point3f(2, 2, 2)));
+		boxes.add(new Box(2, new Point3f(0, 0, 0), new Point3f(3, 3, 3)));
+		Box binTemplate = new Box(0, new Point3f(0, 0, 0), new Point3f(10, 10, 10));
+
+		List<List<Box>> result = solver.solve(boxes, binTemplate);
+
+		// check that both boxes ended in the bin in the same order as in the
+		// original queue
+		assertEquals(1, result.size());
+		assertEquals(2, result.get(0).size());
+
+		assertEquals(boxes.get(0).id, result.get(0).get(0).id);
+		assertEquals(boxes.get(1).id, result.get(0).get(1).id);
+
+	}
+}
